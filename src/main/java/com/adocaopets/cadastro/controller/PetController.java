eef56d02a1ff835efb253350fa11b0c9b53b69a1
AugustoF <ap.fiorucci@gmail.com>
2025-08-20 22:14:24 -0300

    Estruturação PetController

--- src/main/java/com/adocaopets/cadastro/controller/PetController.java
+++ src/main/java/com/adocaopets/cadastro/controller/PetController.java
@@ -1,14 +1,60 @@
 package com.adocaopets.cadastro.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.adocaopets.cadastro.service.PetService;
+import com.adocaopets.cadastro.model.*;
+import com.adocaopets.cadastro.service.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
+@RequestMapping("/pets")
 public class PetController {
 
-    @GetMapping("/Ola")
-    public String hello(){
-        return "Olá  Mundo";
+    private final PetService petService;
+
+    public PetController(PetService petService){
+        this.petService = petService;
     }
+
+    @PostMapping
+    public ResponseEntity<Pet> createPet(@RequestBody Pet pet){
+        Pet created = petService.createPet(pet);
+        return ResponseEntity.ok(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Pet> updatePet(@PathVariable Long id,@RequestBody Pet pet){
+        Pet updated = petService.updatePet(id, pet);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePet(@PathVariable Long id){
+        petService.deletePet(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Pet>> listAll(){
+        return ResponseEntity.ok(petService.listAll());
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Pet>> listByName(@RequestParam String name){
+        return ResponseEntity.ok(petService.listByName(name));
+    }
+
+    @GetMapping("/type")
+    public ResponseEntity<List<Pet>> listByType(@RequestParam PetType type){
+        return ResponseEntity.ok(petService.listByType(type));
+    }
+
+    @GetMapping("/sex")
+    public ResponseEntity<List<Pet>> listByType(@RequestParam SexPet sex){
+        return ResponseEntity.ok(petService.listBySex(sex));
+    }
+
+
 
 }
