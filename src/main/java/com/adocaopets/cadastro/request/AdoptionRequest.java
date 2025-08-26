24c47a77a7ed5ec9b8e43edf4aff3155086d46be
AugustoF <ap.fiorucci@gmail.com>
2025-08-26 13:28:54 -0300

    Criando DTO e Request

--- src/main/java/com/adocaopets/cadastro/request/AdoptionRequest.java
+++ src/main/java/com/adocaopets/cadastro/request/AdoptionRequest.java
@@ -1,4 +1,4 @@
-package com.adocaopets.cadastro.dto;
+package com.adocaopets.cadastro.request;
 
 
 import com.adocaopets.cadastro.model.enums.AdoptionStatus;
---
+++ src/main/java/com/adocaopets/cadastro/request/OwnerRequest.java
@@ -0,0 +1,4 @@
+package com.adocaopets.cadastro.request;
+
+public class OwnerRequest {
+}
