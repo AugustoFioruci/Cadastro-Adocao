180d978cb1748b01dd4395e3e534f45f61581fee
AugustoF <ap.fiorucci@gmail.com>
2025-08-28 14:32:29 -0300

    Estruturando OwnerController pelos padrões de mercado

--- src/main/java/com/adocaopets/cadastro/controller/AdoptionController.java
+++ src/main/java/com/adocaopets/cadastro/controller/AdoptionController.java
@@ -1,4 +1,11 @@
 package com.adocaopets.cadastro.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(name = "adoption")
 public class AdoptionController {
 }
--- src/main/java/com/adocaopets/cadastro/controller/OwnerController.java
+++ src/main/java/com/adocaopets/cadastro/controller/OwnerController.java
@@ -3,10 +3,13 @@ package com.adocaopets.cadastro.controller;
 import com.adocaopets.cadastro.dto.OwnerDTO;
 import com.adocaopets.cadastro.request.OwnerRequest;
 import com.adocaopets.cadastro.service.OwnerService;
-import com.adocaopets.cadastro.model.entity.*;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,19 +20,40 @@ public class OwnerController {
 
 
     @PostMapping
-    public ResponseEntity<Owner> createOwner(@RequestBody Owner owner){
-        return ResponseEntity.ok(owner);
+    public ResponseEntity<OwnerDTO> createOwner(@RequestBody @Valid OwnerRequest request){
+        OwnerDTO owner = ownerService.createOwner(request);
+        return ResponseEntity.created(URI.create("/owners/"+owner.getId())).body(owner);
     }
 
     @PutMapping({"/{id}"})
-    public ResponseEntity<OwnerDTO> updateOwner(@PathVariable Long id, @RequestBody OwnerRequest request){
+    public ResponseEntity<OwnerDTO> updateOwner(@PathVariable Long id, @RequestBody @Valid OwnerRequest request){
         OwnerDTO update = ownerService.updateOwner(id, request);
         return ResponseEntity.ok(update);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteOwner(@PathVariable Long id){
         ownerService.deleteOwner(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OwnerDTO>> listAll(){
+        return ResponseEntity.ok(ownerService.listAll());
+    }
+
+    @GetMapping("/name")
+    public ResponseEntity<List<OwnerDTO>> listByName(String name){
+        return ResponseEntity.ok(ownerService.listByName(name));
+    }
+
+    @GetMapping("/name/contains")
+    public ResponseEntity<List<OwnerDTO>> listByNameContainingIgnoreCase(String name){
+        return ResponseEntity.ok(ownerService.listByNameContainingIgnoreCase(name));
+    }
+
+    @GetMapping("/cpf")
+    public ResponseEntity<List<OwnerDTO>> listByCpf(String cpf){
+        return ResponseEntity.ok(ownerService.listByCpf(cpf));
     }
 }
